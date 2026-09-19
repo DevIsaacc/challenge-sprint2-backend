b16b07b114eb2ead02ffdf1d8e555d870fa8304a
isaac <zacinstyle@gmail.com>
2026-09-19 14:02:10 -0300

    refactor: remove pasta duplicada e deixa os ficheiros na raiz

--- challenge-sprint2-backend/src/main/java/com/aguatrans/challenge_sprint2_backend/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aguatrans.challenge_sprint2_backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.aguatrans.challenge_sprint2_backend.model.Project;
-
-public interface ProjectRepository extends MongoRepository<Project, String> {
-}
